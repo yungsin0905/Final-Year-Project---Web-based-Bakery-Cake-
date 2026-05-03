0da3a7ef089ff559d86a5dae16615ffe7c05a62a
yungsin0905 <wong.yung.sin@student.mmu.edu.my>
2026-05-03 14:56:27 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
- <?php
-  if (session_status() === PHP_SESSION_NONE){
-      session_start();
-  }
- ?>
-
+<?php
+ob_start(); 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
  <!-- search section -->
     <header class="top-bar">
         <div class="logo"><img src="image/cakeology logo.png" alt="Logo" class="img-fluid"></div>
@@ -19,8 +19,13 @@
         <div class="btn-group">
             <a class="save-btn" href="Wishlist.php"><i class="bi bi-heart-fill"></i></a>
             <a class="cart-btn" href="#"><i class="bi bi-cart-fill"></i></a>
-            <a class="profile-btn" href="#"><i class="bi bi-person-circle"></i></a>
-            <a href="sign-up.php" class="sign-up">Sign Up/Login</a>
+            <a class="profile-btn" href="UserDashboard.php"><i class="bi bi-person-circle"></i></a>
+            <?php if(isset($_SESSION['CUSTOMER_ID'])):?>
+                <a href="javascript:void(0);" onclick="confirmLogout()" class="sign-up">Logout</a>
+            <?php else:?>
+                <a href="sign-up.php" class="sign-up">Sign Up/Login</a>
+            <?php endif;?>
+
             <i class ="bi bi-list menu-icon" data-bs-toggle="offcanvas" data-bs-target="#mobileMenu"></i>
         </div>
 
@@ -30,26 +35,25 @@
                 <h5 class="offcanvas-title" id="mobileMenuLabel" style="color: var(--main-color); font-weight: bold;">Menu</h5>
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
               </div>
-              <div class="offcanvas-body">
-                <ul class="list-group list-group-flush">
+                <div class="offcanvas-body">
+                    <ul class="list-group list-group-flush">
                   
-                <li class="list-group-item border-0"><a href="homepage.php" class="text-decoration-none"><i class="bi bi-house me-2"></i> Homepage</a></li>
-                <li class="list-group-item border-0"><a href="UserDashboard.php" class="text-decoration-none"><i class="bi bi-person-circle me-2"></i>User Profile</a></li>
-                <li class="list-group-item border-0"><a href="Wishlist.php" class="text-decoration-none"><i class="bi bi-heart-fill me-2"></i> Wishlist</a></li>
-
-                  <li class="list-group-item border-0"><a href="cart.php" class="text-decoration-none"><i class="bi bi-cart-fill me-2"></i> Shopping Cart</a></li>
-
-                  <li class="list-group-item border-0"><a href="About Us.php" class="text-decoration-none"><i class="bi bi-info-circle me-2"></i> About Us</a></li>
-                  <!-- //if logged in will change the icon to "logged out"
-                  logged out will changed it to "sign up" -->
-                  <?php if(isset($_SESSION['CUSTOMER_ID'])):?>
-                      <li class="list-group-item border-0"><a href="javascript:void(0);" onclick="confirmLogout()" class="text-decoration-none text-danger"><i class="bi bi-box-arrow-in-left me-2"></i> Logout</a></li>
-                    <?php else:?>
-                      <li class="list-group-item border-0"><a href="sign-up.php" class="text-decoration-none"><i class="bi bi-box-arrow-in-right me-2"></i> Sign Up/Sign In</a></li>
-                    <?php endif;?>
-                </ul>
-              </div>
-          </div>
+                        <li class="list-group-item border-0"><a href="homepage.php" class="text-decoration-none"><i class="bi bi-house me-2"></i> Homepage</a></li>
+                        <li class="list-group-item border-0"><a href="UserDashboard.php" class="text-decoration-none"><i class="bi bi-person-circle me-2"></i>User Profile</a></li>
+                        <li class="list-group-item border-0"><a href="Wishlist.php" class="text-decoration-none"><i class="bi bi-heart-fill me-2"></i> Wishlist</a></li>
+                        <li class="list-group-item border-0"><a href="cart.php" class="text-decoration-none"><i class="bi bi-cart-fill me-2"></i> Shopping Cart</a></li>
+                        <li class="list-group-item border-0"><a href="About Us.php" class="text-decoration-none"><i class="bi bi-info-circle me-2"></i> About Us</a></li>
+                         <!-- //if logged in will change the icon to "logged out"
+                        logged out will changed it to "sign up" -->
+                    
+                        <?php if(isset($_SESSION['CUSTOMER_ID'])):?>
+                        <li class="list-group-item border-0"><a href="javascript:void(0);" onclick="confirmLogout()" class="text-decoration-none text-danger"><i class="bi bi-box-arrow-in-left me-2"></i> Logout</a></li>
+                        <?php else:?>
+                        <li class="list-group-item border-0"><a href="sign-up.php" class="text-decoration-none"><i class="bi bi-box-arrow-in-right me-2"></i> Sign Up/Sign In</a></li>
+                        <?php endif;?>
+                    </ul>
+                </div>
+            </div>
     </header>
 
     <!-- navigation section -->
@@ -58,39 +62,39 @@
 
       <!-- dropdown menu -->
       <div class="dropdown-container">
-        <a href="#" class="nav-link-item">All Cakes</a>
+        <a href="product catalogue.php?id=all" class="nav-link-item">All Cakes</a>
         
         <div class="mega-menu">
             <div class="menu-column">
                 <h4>Favourites</h4>
                 <ul>
-                    <li><a href="product catalogue.php">Best Selling</a></li>
-                    <li><a href="product catalogue.php">High Recommended</a></li>
+                    <li><a href="product catalogue.php?cake_type=Best%20Selling">Best Selling</a></li>
+                    <li><a href="product catalogue.php?cake_type=High%20Recommended">High Recommended</a></li>
                 </ul>
             </div>
             <div class="menu-column">
                 <h4>Occasions</h4>
                 <ul>
-                    <li><a href="product catalogue.php">Corporate/ Event</a></li>
-                    <li><a href="product catalogue.php">Birthday Cakes</a></li>
+                    <li><a href="product catalogue.php?id=5">Corporate/ Event</a></li>
+                    <li><a href="product catalogue.php?id=6">Birthday Cakes</a></li>
                 </ul>
             </div>
             <div class="menu-column">
                 <h4>Cake Type</h4>
                 <ul>
                     <li><a href="product catalogue.php?id=all">All Cakes</a></li>
-                    <li><a href="product catalogue.php?id=2">Cheese Cakes</a></li>
-                    <li><a href="product catalogue.php?id=3">Fruit Cakes</a></li>
-                    <li><a href="product catalogue.php?id=4">Gluten-Free Cakes</a></li>
-                    <li><a href="product catalogue.php?id=5">Vegan Cakes</a></li>
+                    <li><a href="product catalogue.php?id=1">Cheese Cakes</a></li>
+                    <li><a href="product catalogue.php?id=2">Fruit Cakes</a></li>
+                    <li><a href="product catalogue.php?id=3">Gluten-Free Cakes</a></li>
+                    <li><a href="product catalogue.php?id=4">Vegan Cakes</a></li>
                 </ul>
             </div>
         </div>
       </div>
 
-      <a href="#">Voucher</a>
-      <a href="#">Customise</a>
-      <a href="#">Membership</a>
+      <a href="voucher.php">Voucher</a>
+      <a href="Customise.php">Customise</a>
+      <a href="membership.php">Membership</a>
     </nav>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
@@ -107,7 +111,7 @@
     //logout
   function confirmLogout(){
   if(confirm("Are you sure you want to logout?"))
-  window.location.href = 'Login.php?logout=true';
+  window.location.href = 'login.php?logout=true';
 }    
 
      // link copy
